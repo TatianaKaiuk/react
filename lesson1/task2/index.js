45a688cdb4447ecca387e4abd52aa60ec17c4066
TatianaKaiuk <persik961243@gmail.com>
2023-04-13 10:55:02 +0300

    .

--- lesson1/task2/index.js
+++ lesson1/task2/index.js
@@ -1,5 +1,6 @@
 const rootElement = document.querySelector('#root');
 
+// если есть вложенность, первый элем создаем и 3 аргумент в виде массива - перечисляем дочерние элементы
 const renderGreeting = React.createElement('div', { className: 'greeting' }, [
   React.createElement('div', { className: 'greeting__title' }, 'Hello, world!'),
   React.createElement(
