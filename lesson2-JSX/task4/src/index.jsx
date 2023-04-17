7b73e0e94e8f6babc03a3ae017599322aea3f5df
TatianaKaiuk <persik961243@gmail.com>
2023-04-17 12:29:57 +0300

    todo list

---
+++ lesson2-JSX/task4/src/index.jsx
@@ -0,0 +1,20 @@
+import './index.css';
+import './styles.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const elemBody = document.querySelector('body');
+
+const element = (
+  <>
+    <h1 className="title">Todo List</h1>
+    <main className="todo-list">
+      <div className="actions">
+        <input className="task-input" type="text" />
+        <button className="btn create-task-btn">Create</button>
+      </div>
+      <ul className="list"></ul>
+    </main>
+  </>
+);
+ReactDOM.render(element, elemBody);
