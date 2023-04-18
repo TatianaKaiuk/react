5dfdef5baad5791d4316e377016f4471b8cb0acf
TatianaKaiuk <persik961243@gmail.com>
2023-04-18 11:21:02 +0300

    .

---
+++ components-props/task1/src/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.scss';
+import Search from './Search';
+
+const elemRoot = document.querySelector('#root');
+
+
+ReactDOM.render(< Search name ="Bob"/>, elemRoot);
