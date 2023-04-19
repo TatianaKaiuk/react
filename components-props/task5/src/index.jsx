ba63000e450dce8c73165c218b7549e87439b005
TatianaKaiuk <persik961243@gmail.com>
2023-04-19 10:11:40 +0300

    .

--- components-props/task5/src/index.jsx
+++ components-props/task5/src/index.jsx
@@ -5,12 +5,12 @@ import Profile from './Profile';
 
 const bodyElem = document.querySelector('body');
 
-const userData = {
-  firstName: 'James',
-  lastName: 'Bond',
-  birthDate: '1991-01-17T11:11:11.819Z',
-  birthPlace: 'London',
-};
+// const userData = {
+//   firstName: 'James',
+//   lastName: 'Bond',
+//   birthDate: '1991-01-17T11:11:11.819Z',
+//   birthPlace: 'London',
+// };
 
 ReactDOM.render(
   <Profile
