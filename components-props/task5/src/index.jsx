d86fdf289f6a43771438779ebadf0663e18d2b60
TatianaKaiuk <persik961243@gmail.com>
2023-04-19 10:10:34 +0300

    .

--- components-props/task5/src/index.jsx
+++ components-props/task5/src/index.jsx
@@ -16,7 +16,7 @@ ReactDOM.render(
   <Profile
     firstName="James"
     lastName="Bond"
-    birthDate="1991-01-17T11:11:11.819Z"
+    birthDate={"1991-01-17T11:11:11.819Z"}
     birthPlace="London"
   />,
   bodyElem
