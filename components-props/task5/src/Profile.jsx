840be0f612278a8a96cb8669ea5ca0fe978b9409
TatianaKaiuk <persik961243@gmail.com>
2023-04-19 10:14:51 +0300

    .

--- components-props/task5/src/Profile.jsx
+++ components-props/task5/src/Profile.jsx
@@ -3,13 +3,13 @@ import moment from 'moment';
 
 const formDate = (date) => moment(date).format('DD MMM YY');
 
-const Profile = (props) => {
+const Profile = ({ firstName, lastName, birthDate, birthPlace }) => {
   return (
     <>
-      <div className="profile__name">{`${props.firstName} ${props.lastName}`}</div>
+      <div className="profile__name">{`${firstName} ${lastName}`}</div>
       <div className="profile__birth">{`Was born ${formDate(
-        props.birthDate
-      )} in ${props.birthPlace}`}</div>
+        birthDate
+      )} in ${birthPlace}`}</div>
     </>
   );
 };
