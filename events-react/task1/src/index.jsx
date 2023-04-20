d5732ec827ece54ef7547bdc20ae70bdf70dc3ed
TatianaKaiuk <persik961243@gmail.com>
2023-04-20 18:54:37 +0300

    event

---
+++ events-react/task1/src/index.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import GoodButton from './GoodButton';
+import './index.scss';
+
+const bodyElem = document.querySelector('body');
+ ReactDOM.render(<GoodButton />, bodyElem);
