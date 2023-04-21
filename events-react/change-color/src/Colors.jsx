6e2c3d2e7198d899408ce4579a9d39634ca61960
TatianaKaiuk <persik961243@gmail.com>
2023-04-21 09:40:33 +0300

    change color with react

---
+++ events-react/change-color/src/Colors.jsx
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+
+const RED = '#f00';
+const GREEN = '#0f0';
+const BLUE = '#00f';
+
+// изменяем цвет тела
+class Colors extends Component {
+
+  setBodyColor= color => {
+    document.body.style.backgroundColor = color;  // получаем доступ к body, а именно к бекграунту и устанавливаем ему цвет, который передаем как аргум
+  }
+
+  render() {
+    // отрисовка- стиль style={{ backgroundColor: RED }} только в таком виде
+    // обраработчик событий onClick - помещаем функц, при использовании bind, доп возвращает event событие, его использовать
+    // если нужен event, если нет - просто стрелочную функцию
+    // при клике, срабатывает функц setBodyColor и меняет backgroundColor
+    return (
+      <div className="colors">
+        <button
+          style={{ backgroundColor: RED }}
+          onClick={this.setBodyColor.bind(this, RED)}
+          className="colors__button"
+        ></button>
+
+        <button
+          style={{ backgroundColor: GREEN }}
+          onClick={() => this.setBodyColor(GREEN)}
+          className="colors__button"
+        ></button>
+
+        <button
+          style={{ backgroundColor: BLUE }}
+          onClick={() => this.setBodyColor(BLUE)}
+          className="colors__button"
+        ></button>
+      </div>
+    );
+  }
+}
+export default Colors;
