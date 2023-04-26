58f0c608917e3fa45145188de383619c5f2680b6
TatianaKaiuk <persik961243@gmail.com>
2023-04-26 13:06:06 +0300

    .

--- rendering/render-text/src/Page.jsx
+++ rendering/render-text/src/Page.jsx
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 import Info from './Info';
 
-const IPhone13 = 'Price is 500$. Available in 2 colors';
-const  IPhone13Pro = 'Price is 650$. Not available in 1 color';
+const text1 = 'Price is 500$. Available in 2 colors';
+const  text2 = 'Price is 650$. Not available in 1 color';
 
 class Page extends Component {
     state = { // конструктор используем если нужен доступ к props
@@ -20,10 +20,10 @@ class Page extends Component {
           <div className="page">
             <Info info={this.state.info} />
             <div className="actions">
-              <button className="btn" onClick={() => this.setInfo(IPhone13)}>
+              <button className="btn" onClick={() => this.setInfo(text1)}>
                 IPhone 13
               </button>
-              <button className="btn" onClick={() => this.setInfo(IPhone13Pro)}>
+              <button className="btn" onClick={() => this.setInfo(text2)}>
                 IPhone 13 Pro
               </button>
               <button className="btn" onClick={() => this.setInfo('')}>
