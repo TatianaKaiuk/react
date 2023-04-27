c8b4245dc2ee0c4abcea8182bf114ff30d1f9be2
TatianaKaiuk <persik961243@gmail.com>
2023-04-27 14:50:37 +0300

    .

--- rendering/component-avtorizacia/src/Spinner.jsx
+++ rendering/component-avtorizacia/src/Spinner.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
 const Spinner = ({ size }) => {
-  return <span className="spinner" style={ { radius: {size} } }></span>;
+  return <span className="spinner" style={ { height: size, width: size } }></span>;
 };
 export default Spinner;
