2bd41ac0e2a751020f8a9915c943dd4056012f2e
TatianaKaiuk <persik961243@gmail.com>
2023-05-04 11:01:19 +0300

    .

--- FORMS/userList-filter/src/Filter.jsx
+++ FORMS/userList-filter/src/Filter.jsx
@@ -1,17 +1,17 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-const Filter = ({FilterText, count, onChange})=> {
+const Filter = ({filterText, count, onChange})=> {
     const handleFilterChange = (e) => {
       onChange(e.target.value);
     };
-    
+
     return (
       <div className="filter">
         <span className="filter__count">{count}</span>
         <input
           type="text"
           className="filter__input"
-          value={FilterText}
+          value={filterText}
           onChange={handleFilterChange}
         />
       </div>
