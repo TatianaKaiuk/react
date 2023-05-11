67b3b4d42a2142d99ab7cbd557a3d35281c68bcd
TatianaKaiuk <persik961243@gmail.com>
2023-05-11 13:27:49 +0300

    state up

---
+++ lifting-state-up/userProfile/src/App.jsx
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import UserMenu from './UserMenu';
+import UserProfile from './UserProfile';
+
+class App extends Component {
+  state = {
+    userData: null,
+  };
+  componentDidMount() {
+    this.fetchUserData(this.props.userId);
+  }
+  fetchUserData = (userId) => {
+    const userUrl = `https://api.github.com/users/${userId}`;
+    fetch(userUrl)
+      .then((response) => response.json())
+      .then((userData) =>
+        this.setState({
+          userData,
+        })
+      );
+  };
+
+  render() {
+    return (
+      <div className="page">
+        <header className="header">
+          <UserMenu userData={this.state.userData} />
+        </header>
+        <UserProfile userData={this.state.userData} />
+      </div>
+    );
+  }
+}
+export default App;
