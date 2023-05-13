816d75c22621183bce3cbd23b65927912679109f
TatianaKaiuk <persik961243@gmail.com>
2023-05-13 11:52:39 +0300

    .

---
+++ CalcTemper-wather/src/index.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+// import './index.scss';
+import Calculator from './Calculator';
+
+const elemRoot = document.querySelector('#root');
+ReactDOM.render(<Calculator />, elemRoot);
