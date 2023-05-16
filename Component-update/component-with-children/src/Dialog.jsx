2986e6ad837588428ee1b5158b2e4e44514a38bb
TatianaKaiuk <persik961243@gmail.com>
2023-05-16 13:29:23 +0300

    .

--- Component-update/component-with-children/src/Dialog.jsx
+++ Component-update/component-with-children/src/Dialog.jsx
@@ -7,12 +7,12 @@ const Dialog = ({children, isOpen, title, onClose}) => {
     }
 
         return (
-          <div class="dialog">
-            <div class="dialog__heading">
-              <h4 class="dialog__title">{title}</h4>
-              <button class="dialog__close-btn" onClick={onClose}>+</button>
+          <div className="dialog">
+            <div className="dialog__heading">
+              <h4 className="dialog__title">{title}</h4>
+              <button className="dialog__close-btn" onClick={onClose}>+</button>
             </div>
-            <div class="dialog__content">{children} </div>
+            <div className="dialog__content">{children} </div>
           </div>
         );
 }
