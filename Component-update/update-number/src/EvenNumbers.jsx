577e5508190596cfb4181d14c6eada265376d74b
TatianaKaiuk <persik961243@gmail.com>
2023-05-16 12:45:15 +0300

    .

--- Component-update/update-number/src/EvenNumbers.jsx
+++ Component-update/update-number/src/EvenNumbers.jsx
@@ -4,7 +4,7 @@ class EvenNumbers extends Component {
   shouldComponentUpdate(nextProps) {
     // this.props.number = 70
     // nextProps.number = 71
-    return nextProps.number % 2 === 0;
+    return nextProps.number % 2 === 1;
   }
 
   render() {
