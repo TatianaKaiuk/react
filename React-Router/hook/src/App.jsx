ca86d10bc04446b6f5f80ffea613612f4fd18182
TatianaKaiuk <persik961243@gmail.com>
2023-06-19 15:25:57 +0300

    .

--- React-Router/hook/src/App.jsx
+++ React-Router/hook/src/App.jsx
@@ -1,29 +1,29 @@
 import React from 'react';
-import { Switch, BrowserRouter, Route, Link } from 'react-router-dom';
+import { Switch, BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import User from './User';
 
 const App = () => {
   return (
-    <div className="page__content">
-      <h1>Users</h1>
-      <BrowserRouter>
-        <ul className="navigation">
-          <li className="navigation__item">
-            <Link to="/users/github">Github</Link>
-          </li>
-          <li className="navigation__item">
-            <Link to="/users/facebook">Facebook</Link>
-          </li>
-        </ul>
-        <Switch>
-          <Route exact path="/users">
-            <span>Select a user please</span>
-          </Route>
-          <Route path="/users/:userId">
-            <User />
-          </Route>
-        </Switch>
-      </BrowserRouter>
+    <div className="page">
+      <Router>
+        <div className="page__content">
+          <h1>Users</h1>
+          <ul className="navigation">
+            <li className="navigation__item">
+              <Link to="/users/github">Github</Link>
+            </li>
+            <li className="navigation__item">
+              <Link to="/users/facebook">Facebook</Link>
+            </li>
+          </ul>
+          <Switch>
+            <Route path="/users/:userId" component={User} />
+            <Route  path="/">
+              <span>Select a user please</span>
+            </Route>
+          </Switch>
+        </div>
+      </Router>
     </div>
   );
 };
